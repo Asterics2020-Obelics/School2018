49f9192f47fdba8089d99f3db5a3a8081b9b6a0f
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2018-06-03 20:43:21 +0200

    Use shorter form for argparse setup

--- codingstyle/parser.py
+++ codingstyle/parser.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Example with non-optional arguments')
 
-parser.add_argument('count', action="store", type=int)
-parser.add_argument('units', action="store")
+parser.add_argument('count', type=int)
+parser.add_argument('units')
 
 print(parser.parse_args())
