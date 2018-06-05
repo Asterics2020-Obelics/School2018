517abcb73ca0f3f538b6d5346fe857578621d8bb
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-06-05 14:44:26 +0200

    Add formula

--- matplotlib-publication-quality/plot.py
+++ matplotlib-publication-quality/plot.py
@@ -10,5 +10,13 @@ plt.plot(t, np.sin(2 * np.pi * f * t))
 plt.xlabel('$t / \mathrm{s}$')
 plt.ylabel('$U / \mathrm{V}$')
 
+plt.text(
+    0.5, 1.05,
+    r'$U(t) = \sin(2 \pi \nu t)$',
+    va='bottom',
+    ha='center'
+)
+plt.ylim(-1.1, 1.2)
+
 plt.tight_layout(pad=0)
 plt.savefig('plot.pdf')
