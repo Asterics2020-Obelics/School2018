5926c81bb574986441665605734cf004a5ccef87
David Kirkby <dkirkby@uci.edu>
2018-06-07 11:49:08 +0200

    Update ML support pkg

--- machinelearning/mls/__init__.py
+++ machinelearning/mls/__init__.py
@@ -1,2 +1,3 @@
 from .plot import display_gmm, plot_image, plot_classification, scan_misclassified, plot_regression
 from .mcmc import MCMC_sample
+from .neural import nn_unit_draw2d, nn_graph_draw2d
